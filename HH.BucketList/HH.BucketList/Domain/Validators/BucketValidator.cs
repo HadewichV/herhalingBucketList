43a272be8911eaeae7849abddc7a68c5db1959b0
Hade Vanhoutte <hade.vanhoutte@outlook.be>
2019-04-16 23:16:55 +0200

    Commit 2 - validation

---
+++ HH.BucketList/HH.BucketList/Domain/Validators/BucketValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using HH.BucketList.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HH.BucketList.Domain.Validators
+{
+    public class BucketValidator: AbstractValidator<BucketL>
+    {
+        public BucketValidator()
+        {
+            RuleFor(bucketL => bucketL.Title)
+                .NotEmpty()
+                .WithMessage("Title cannot be empty")
+                .Length(2, 20)
+                .WithMessage("Length must be between 2 and 20");
+
+            RuleFor(bucketL => bucketL.Description)
+                .NotEqual(b => b.Title)
+                .WithMessage("Description must be different from Title")
+                .NotEmpty()
+                .WithMessage("Description cannot be empty");            
+        }
+    }
+}
--- HH.BucketList/HH.BucketList/Views/BucketView.xaml.cs
+++ HH.BucketList/HH.BucketList/Views/BucketView.xaml.cs
@@ -1,5 +1,7 @@
-﻿using HH.BucketList.Domain.Models;
+﻿using FluentValidation;
+using HH.BucketList.Domain.Models;
 using HH.BucketList.Domain.Services;
+using HH.BucketList.Domain.Validators;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +19,7 @@ namespace HH.BucketList.Views
         private BucketsInMemoryService bucketService;
         private AppSettingsInMemoryService settingsService;
         private BucketL currentBucketL;
+        private IValidator bucketValidator;
 
 		public BucketView (BucketL bucketL)
 		{
@@ -24,6 +27,7 @@ namespace HH.BucketList.Views
 
             settingsService = new AppSettingsInMemoryService();
             bucketService = new BucketsInMemoryService();
+            bucketValidator = new BucketValidator();
 
             if (bucketL == null)
             {
@@ -54,10 +58,13 @@ namespace HH.BucketList.Views
         private async void BtnSave_Clicked(object sender, EventArgs e)
         {
             SaveBucketState();
-            busyIndicator.IsVisible = true;
-            await bucketService.SaveBucketList(currentBucketL);
-            busyIndicator.IsVisible = false;
-            await DisplayAlert("Saved", $"Your bucket list {currentBucketL.Title} has been saved", "Ok");
+            if (Validate(currentBucketL))
+            {
+                busyIndicator.IsVisible = true;
+                await bucketService.SaveBucketList(currentBucketL);
+                busyIndicator.IsVisible = false;
+                await DisplayAlert("Saved", $"Your bucket list {currentBucketL.Title} has been saved", "Ok");
+            }
         }
 
         private void SaveBucketState()
@@ -65,6 +72,57 @@ namespace HH.BucketList.Views
             currentBucketL.Title = txtTitle.Text;
             currentBucketL.Description = txtDescription.Text;
             currentBucketL.IsFavorite = swIsFavorite.IsToggled;
+        }
+
+        private bool Validate(BucketL bucketL)
+        {
+            lblErrorTitle.IsVisible = false;
+            lblErrorDescription.IsVisible = false;
+
+            var validationResult = bucketValidator.Validate(bucketL);
+
+            //loop through error to identify properties
+            foreach(var error in validationResult.Errors)
+            {
+                if (error.PropertyName == nameof(bucketL.Title))
+                {
+                    lblErrorTitle.Text = error.ErrorMessage;
+                    lblErrorTitle.IsVisible = true;
+                }
+
+                if (error.PropertyName == nameof(bucketL.Description))
+                {
+                    lblErrorDescription.Text = error.ErrorMessage;
+                    lblErrorDescription.IsVisible = true;
+                }
+            }
+            return validationResult.IsValid;
+
+            //bool error = false;
+
+            //if (string.IsNullOrWhiteSpace(bucketL.Title))
+            //{
+            //    error = true;
+            //    lblErrorTitle.Text = "Title cannot be empty.";
+            //    lblErrorTitle.IsVisible = true;
+            //}
+
+            //if (string.IsNullOrWhiteSpace(bucketL.Description))
+            //{
+            //    error = true;
+            //    lblErrorDescription.Text = "Description cannot be empty.";
+            //    lblErrorDescription.IsVisible = true;
+            //}
+
+            //if (!error)
+            //{
+            //    lblErrorTitle.Text = "";
+            //    lblErrorTitle.IsVisible = false;
+            //    lblErrorDescription.Text = "";
+            //    lblErrorDescription.IsVisible = false;
+            //}
+
+            //return !error;
         }
     }
 }
